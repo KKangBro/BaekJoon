24d7072825b2d6b955bec7f9df9b33a4e02b69af
kky417 <kky417@KKangBro-X1>
2023-03-29 23:06:11 +0900

    0329 home

--- src/n11659/Main.java
+++ src/n11659/Main.java
@@ -3,22 +3,21 @@ package n11659;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.StringReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
 	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 	static StringBuilder output = new StringBuilder();
-//	static String src = """
-//			5 3
-//			5 4 3 2 1
-//			1 3
-//			2 4
-//			5 5
-//			"""; // output: 12 9 1
+	static String src = """
+			5 3
+			5 4 3 2 1
+			1 3
+			2 4
+			5 5
+			"""; // output: 12 9 1
 
 	public static void main(String[] args) throws Exception {
-//		input = new BufferedReader(new StringReader(src));
+		input = new BufferedReader(new StringReader(src));
 
 		StringTokenizer st = new StringTokenizer(input.readLine());
 		int n = Integer.parseInt(st.nextToken());
