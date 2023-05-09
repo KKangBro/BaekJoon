7c6f11ff6b7e976a84a7aba3628cd10e7992b273
KKangBro <417kky@gmail.com>
2023-05-10 00:40:39 +0900

    0509 home

--- src/n11403/Main.java
+++ src/n11403/Main.java
@@ -1,31 +1,30 @@
-package n11403;
+package n11403; // 경로 찾기
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.StringReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
-
-// 경로 찾기
 
 public class Main {
 	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 	static StringBuilder output = new StringBuilder();
 	static String src = """
-			3
-			0 1 0
-			0 0 1
-			1 0 0
+			7
+			0 0 0 1 0 0 0
+			0 0 0 0 0 0 1
+			0 0 0 0 0 0 0
+			0 0 0 0 1 1 0
+			1 0 0 0 0 0 0
+			0 0 0 0 0 0 1
+			0 0 1 0 0 0 0
 			"""; // output: 1 1 1\n1 1 1\n1 1 1
 	static int[][] map;
-	static int[][] ans;
 
 	public static void main(String[] args) throws Exception {
 		input = new BufferedReader(new StringReader(src));
 
 		int N = Integer.parseInt(input.readLine());
 		map = new int[N][N];
-		ans = new int[N][N];
 		StringTokenizer st;
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(input.readLine());
@@ -33,13 +32,25 @@ public class Main {
 				map[i][j] = Integer.parseInt(st.nextToken());
 		}
 		
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				
+		for (int k = 0; k < N; k++) {
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					if(i == k | j == k)
+						continue;
+					
+					if(map[i][k] == 1 && map[k][j] == 1) {
+						map[i][j] = 1;
+					}
+				}
 			}
 		}
 		
-		
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				output.append(map[i][j]+" ");
+			}
+			output.append("\n");
+		}
+		System.out.println(output);
 	}
-
 }
